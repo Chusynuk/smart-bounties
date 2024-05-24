760fa051431cb76ccea287fdb4b12578075f94a8
eloi <recycled_loops@hotmail.com>
2024-05-24 20:08:02 +0200

    initial project setup

---
+++ src/lib/config.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { type Chain, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { http, cookieStorage, createStorage } from "wagmi";
+import { blastSepolia, bscTestnet, sepolia } from "wagmi/chains";
+
+const projectId = "a69e1566c04e28f4ae629b951c8b6736";
+
+const supportedChains: Chain[] = [sepolia, bscTestnet, blastSepolia];
+
+export const config = getDefaultConfig({
+	appName: "WalletConnection",
+	projectId,
+	chains: supportedChains as any,
+	ssr: true,
+	storage: createStorage({
+		storage: cookieStorage,
+	}),
+	transports: supportedChains.reduce(
+		(obj, chain) => ({ ...obj, [chain.id]: http() }),
+		{},
+	),
+});
